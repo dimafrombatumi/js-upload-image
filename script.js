3289d057c0b0503e8c827f838aad3a7226b37ca4
dimafrombatumi <dimafrombatumi@gmail.com>
2020-05-13 13:57:50 +0400

    added random images loading function (with images)

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+let urlText = document.getElementById('urlText');
+let urlInput = document
+    .getElementById('urlInput')
+    .value;
+let imageCont = document.getElementById('avatarId');
 function imgChange() {
-    let urlText = document.getElementById('urlText');
 
-    let urlInput = document
-        .getElementById('urlInput')
-        .value;
-    let imageCont = document.getElementById('avatarId');
     imageCont.src = urlInput;
     urlText.href = urlInput;
+}
+
+function imgRand() {
+    const id = Math.round(Math.random() * (10 - 5)) + 5;
+    let imageScr = `img/${id}.jpg`;
+    imageCont.src = imageScr;
 }
