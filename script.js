acaf4ba2d165839cf4ab70ea1c16936beb80ab95
dimafrombatumi <dimafrombatumi@gmail.com>
2020-05-13 11:34:15 +0400

    added  link to image in the end of page

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function imgChange() {
+    let urlText = document.getElementById('urlText');
 
     let urlInput = document
         .getElementById('urlInput')
         .value;
     let imageCont = document.getElementById('avatarId');
     imageCont.src = urlInput;
-    let isUpload = true;
+    urlText.href = urlInput;
 }
-
-document.getElementById("test").innerHTML = '<ul><li>1</li><li>2</li><li>3</li></ul>';
